6e5adb004eb70156dba1caeb20dbdef26509cfe3
Maria Romanova <aemulare@users.noreply.github.com>
2017-07-18 14:43:10 -0400

    Modifies script to updates coin quantities in the cart. Resizes images.

--- js/myscripts.js
+++ js/myscripts.js
@@ -9,11 +9,18 @@ $(document).ready(function(){
     }
 
     var theTotal = 0;
-    var idClicked, currentPrice, parentCard, coinName, parentCardId;
+    var coinQuantity = 0;
+    var idClicked, currentPrice, parentCard, coinName, parentCardId, btnIndex;
+    var itemQuantities = new Array(10).fill(0);
 
     $("button").click(function(e){
         idClicked = this.id;
         coinPrice = this.value;
+
+        btnIndex = Number(idClicked.substring(4));
+        itemQuantities[btnIndex] += 1;
+        coinQuantity = itemQuantities[btnIndex];
+
 
         parentCard = $(this).parents().eq(2);
         // alert(parentCard.prop("tagName") + " class: " + parentCard.prop("className") + ", id = " + parentCard.prop("id"));
@@ -24,13 +31,17 @@ $(document).ready(function(){
         coinDescription = $(parentCardId).find("span").eq(1).text();
         coinImg = $(parentCardId).find("img").eq(0).attr("src");
 
-
-        $(".dropdown-content").append("<li><table><tr>" +
-            "<td><img class='coin' src='" + coinImg + "'></td>" +
-            "<td>"+coinName+"</td>" +
-            "<td>1x</td>" +
-            "<td>"+coinPrice+"</td>" +
-        "</tr></table></li>");
+        if (coinQuantity === 1) {
+            $(".dropdown-content").append("<li><table><tr>" +
+                "<td><img class='coin' src='" + coinImg + "'></td>" +
+                "<td>" + coinName + "</td>" +
+                "<td>" + itemQuantities[btnIndex] + "x</td>" +
+                "<td>" + coinPrice + "</td>" +
+                "</tr></table></li>");
+        }
+        else {
+            $(".dropdown-content table td:nth-child(3)").text(coinQuantity + "x");
+        }
 
         // alert("You clicked button: " + idClicked + ", item price = " + coinPrice);
 
@@ -39,7 +50,11 @@ $(document).ready(function(){
 
     }); // end click function
 
+
 }); // end ready function
+
+
+
 
 
 
